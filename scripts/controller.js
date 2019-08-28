0000c7b85735805c4f8f66f5f4702a2414db5161
futureCode16 <2ndyrGroupC@pn.local>
2019-08-28 16:07:54 +0800

    Modify the Code

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,17 +1,68 @@
 // basic functionalities
-client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
-client.subscribe("mqtt/demo")
+var address = document.getElementById('address');
+var subscribe = document.getElementById('subscribeTopic');
+var connect = document.getElementById('btnConnect');
+var disconnect = document.getElementById('btnDisconnect');
+var message = document.getElementById('payload');
+var publishTopic = document.getElementById('publishTopic');
+var publishBtn = document.getElementById('btnPublish');
+var subscribeBtn = document.getElementById('btnSubscribe');
+var unsubscribeBtn = document.getElementById('btnUnsubscribe');
 
-client.on("connect", function(){
-    console.log("Successfully connected");
+connect.addEventListener('click', function (prevent) {
+  prevent.preventDefault();
+  client = mqtt.connect(address.value)
+
+  subscribeBtn.addEventListener('click', function (prevent) {
+    prevent.preventDefault();
+
+    client.subscribe("mqtt/" + subscribe.value)
+  });
+
+  unsubscribeBtn.addEventListener('click',function(prevent){
+    prevent.preventDefault();
+
+    client.unsubscribe("mqtt/" + subscribe.value);
+  });
+
+  client.on("connect", function () {
+    var status = document.getElementById('status');
+    status.innerHTML = "Connected Successfully!";
+  });
+
+  client.on("message", function (topic, payload) {
+    console.log([topic, payload].join(": "));
+    var timeStamp = new Date();
+    console.log(timeStamp);
+    var tableBody = document.getElementById('tablebody');
+    var tr = document.createElement('tr');
+    var MessageTopic = document.createElement('td');
+    var MessagePayload = document.createElement('td');
+    var MessageTimeStamp = document.createElement('td');
+    MessageTopic.appendChild(document.createTextNode(subscribe.value));
+    MessagePayload.appendChild(document.createTextNode(payload));
+    MessageTimeStamp.appendChild(document.createTextNode(timeStamp));
+    tr.appendChild(MessageTopic);
+    tr.appendChild(MessagePayload);
+    tr.appendChild(MessageTimeStamp);
+    tableBody.appendChild(tr);
+
+  });
+
+  publishBtn.addEventListener('click', function (prevent) {
+    prevent.preventDefault();
+
+    client.publish("mqtt/" + publishTopic.value, message.value);
+  })
+
+  disconnect.addEventListener('click', function () {
+    var status = document.getElementById('status');
+    status.innerHTML = "Disconnected!";
+    client.end();
+  });
+
 })
 
-client.on("message", function (topic, payload) {
-  console.log([topic, payload].join(": "));
-  client.end();
-})
-
-client.publish("mqtt/demo", "hello world!")
 
 // // advance functionalities
 // client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
